307ac69cb2b32991ea74316166dc73625d5e6ab5
expldec <explosive.deconstruction@gmail.com>
2022-05-19 18:54:40 +0200

    implemented loader component

--- src/components/AppListView.vue
+++ src/components/AppListView.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="app-list__container container">
+  <AppLoading v-if="loading" />
+  <div v-else class="app-list__container container">
     <div class="row row-cols-2 row-cols-md-4 g-2">
       <AppAlbumCard v-for="(item, index) in discList" :key="index" :album="item" />
     </div>
@@ -8,21 +9,27 @@
 
 <script>
 import AppAlbumCard from "./AppAlbumCard.vue";
+import AppLoading from "./AppLoading.vue";
 import axios from "axios";
 
 export default {
   name: "AppListView",
   components: {
     AppAlbumCard,
+    AppLoading,
   },
   data() {
     return {
       discList: [],
+      loading: true,
     };
   },
   created() {
     axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((resp) => {
       this.discList = resp.data.response;
+      setTimeout(() => {
+        this.loading = false;
+      }, 500);
     });
   },
 };
