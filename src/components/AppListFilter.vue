d36e32628b3a5ea515afb43f67143a384a0dd591
expldec <explosive.deconstruction@gmail.com>
2022-05-20 19:47:57 +0200

    implemented genre filtering component and functions

---
+++ src/components/AppListFilter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="form-group flex-grow-1">
+    <select class="form-select" aria-label="Genre Select" v-model="genreSelect" @change="$emit('pickedGenre', genreSelect)">
+        <option disabled selected value>Select genre</option>
+        <option value="rock">Rock</option>
+        <option value="pop">Pop</option>
+        <option value="jazz">Jazz</option>
+        <option value="metal">Metal</option>
+    </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppListFilter',
+    data: function() {
+        return {
+            genreSelect: '',
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/AppListView.vue
+++ src/components/AppListView.vue
@@ -1,8 +1,11 @@
 <template>
   <AppLoading v-if="loading" />
   <div v-else class="app-list__container container">
+    <div class="row row-cols-1">
+      <AppListFilter @pickedGenre="genreFilter($event)" />
+    </div>
     <div class="row row-cols-2 row-cols-md-4 g-2">
-      <AppAlbumCard v-for="(item, index) in discList" :key="index" :album="item" />
+      <AppAlbumCard v-for="(item, index) in filteredAlbums" :key="index" :album="item" />
     </div>
   </div>
 </template>
@@ -10,6 +13,7 @@
 <script>
 import AppAlbumCard from "./AppAlbumCard.vue";
 import AppLoading from "./AppLoading.vue";
+import AppListFilter from "./AppListFilter.vue";
 import axios from "axios";
 
 export default {
@@ -17,12 +21,30 @@ export default {
   components: {
     AppAlbumCard,
     AppLoading,
+    AppListFilter,
   },
   data() {
     return {
       discList: [],
       loading: true,
+      filter: {
+        genre: '',
+      }
     };
+  },
+  methods: {
+    genreFilter(genre) {
+      this.filter.genre = genre;
+    },
+  },
+  computed: {
+    filteredAlbums: function() {
+      const genreFormatted = this.filter.genre.toLowerCase();
+      const filteredAlbums = this.discList.filter((item) => {
+        return item.genre.toLowerCase().includes(genreFormatted);
+      });
+      return filteredAlbums;
+    },
   },
   created() {
     axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((resp) => {
