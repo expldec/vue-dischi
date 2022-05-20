a6b15f385a8e7b819b4292d348c42583ea9ae76b
expldec <explosive.deconstruction@gmail.com>
2022-05-20 20:57:14 +0200

    added artist filter with dynamically created artist array, refactored filtering method to make it generic

--- src/components/AppListFilter.vue
+++ src/components/AppListFilter.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="form-group flex-grow-1">
-    <select class="form-select" aria-label="Genre Select" v-model="genreSelect" @change="$emit('pickedGenre', genreSelect)">
-        <option selected value>All genres</option>
-        <option value="rock">Rock</option>
-        <option value="pop">Pop</option>
-        <option value="jazz">Jazz</option>
-        <option value="metal">Metal</option>
-    </select>
+  <div class="row row-cols-2 row-cols-md-4 g-2">
+    <div class="form-group flex-grow-1">
+      <select class="form-select d-flex" aria-label="Genre Select" v-model="genreSelect" @change="$emit('pickedGenre', genreSelect)">
+          <option selected value>All genres</option>
+          <option value="rock">Rock</option>
+          <option value="pop">Pop</option>
+          <option value="jazz">Jazz</option>
+          <option value="metal">Metal</option>
+      </select>
     </div>
+    <div class="form-group flex-grow-1">
+      <select class="form-select d-flex" aria-label="Artist Select" v-model="artistSelect" @change="$emit('pickedArtist', artistSelect)">
+          <option selected value>All artists</option>
+          <option v-for="(item,index) in artists" :key="index" :value="item.toLowerCase()">{{ item }}</option>
+      </select>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -16,9 +24,13 @@ export default {
     data: function() {
         return {
             genreSelect: '',
+            artistSelect: '',
         }
     },
     methods: {
+    },
+    props: {
+        artists: Array,
     }
 }
 </script>
--- src/components/AppListView.vue
+++ src/components/AppListView.vue
@@ -1,9 +1,7 @@
 <template>
   <AppLoading v-if="loading" />
   <div v-else class="app-list__container container">
-    <div class="row row-cols-1">
-      <AppListFilter @pickedGenre="genreFilter($event)" />
-    </div>
+      <AppListFilter @pickedGenre="updateFilter('genre',$event)" @pickedArtist="updateFilter('artist',$event)" :artists="artistArray"/>
     <div class="row row-cols-2 row-cols-md-4 g-2">
       <AppAlbumCard v-for="(item, index) in filteredAlbums" :key="index" :album="item" />
     </div>
@@ -29,21 +27,32 @@ export default {
       loading: true,
       filter: {
         genre: '',
+        artist: '',
       }
     };
   },
   methods: {
-    genreFilter(genre) {
-      this.filter.genre = genre;
+    updateFilter(key, filter) {
+      this.filter[key] = filter;
     },
   },
   computed: {
     filteredAlbums: function() {
-      const genreFormatted = this.filter.genre.toLowerCase();
       const filteredAlbums = this.discList.filter((item) => {
-        return item.genre.toLowerCase().includes(genreFormatted);
+        return (item.genre.toLowerCase().includes(this.filter.genre) && item.author.toLowerCase().includes(this.filter.artist));
       });
       return filteredAlbums;
+    },
+    artistArray: function() {
+      const artistArray = this.discList.reduce(       // qui ho voluto usare reduce per creare l'array degli artisti
+        (previous, current) => {                      // a ogni giro del reduce, prende il risultato del giro prima
+          if (!previous.includes(current.author)) {   // se l'array che stiamo costruendo NON include già l'artista di questo giro
+            previous.push(current.author);            //    Pushamo l'artista nell'array
+            return previous;                          // passiamo l'array così modificato al prossimo giro del reduce
+          }
+        }, []                                         // valore del previous nel primo giro (array vuoto)
+      );
+      return artistArray
     },
   },
   created() {
